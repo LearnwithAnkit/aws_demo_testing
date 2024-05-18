30c0dc52ef87a529be326083e92b14867d8f9f02
Ankit <ankit.anshu145@gmail.com>
2024-05-18 10:10:06 +0530

    Updated test

--- src/main/java/com/example/demo/controllet/TestController.java
+++ src/main/java/com/example/demo/controllet/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Tayaar ho jao";
     }
 
 }
